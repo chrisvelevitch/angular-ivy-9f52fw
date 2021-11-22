460dee94bdfa0563c26d33367a48501f3ed9c5e4
chrisvelevitch <chris.velevitch@gmail.com>
2021-11-22 17:10:05 +0800

    4 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,50 +1,11 @@
-import { Component, VERSION } from '@angular/core';
-import { DataSource } from '@angular/cdk/collections';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Component, Input, VERSION } from '@angular/core';
 
-@Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
-})
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
-
-  columns = [
-    'Date',
-    'Text'
-  ];
-
-  export interface PeriodicElement {
-    date: string;
-    text: number;
-    weight: number;
-    symbol: string;
-  }
-
-  const MY_DATA: PeriodicElement[] = [
-    {date: 1, text: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-    {date: 2, text: 'Helium', weight: 4.0026, symbol: 'He'},
-    {date: 3, text: 'Lithium', weight: 6.941, symbol: 'Li'},
-    // {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    // {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-    // {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-    // {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-    // {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-    // {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-    // {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  ];
-  
-}
-
-export class ExampleDataSource extends DataSource<PeriodicElement> {
-  /** Stream of data that is provided to the table. */
-  data = new BehaviorSubject<PeriodicElement[]>(ELEMENT_DATA);
-
-  /** Connect function called by the table to retrieve one stream containing the data to render. */
-  connect(): Observable<PeriodicElement[]> {
-    return this.data;
-  }
-
-  disconnect() {}
+export class AppComponent {
+  @Component({
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+  })
+  @Input()
+  name: string = 'Angular ' + VERSION.major;
 }
--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,10 +1,44 @@
 import { Component, Input } from '@angular/core';
+import { DataSource } from '@angular/cdk/collections';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+export interface PeriodicElement {
+  date: number;
+  text: string;
+  weight: number;
+  symbol: string;
+}
+
+const MY_DATA: PeriodicElement[] = [
+  { date: 1, text: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { date: 2, text: 'Helium', weight: 4.0026, symbol: 'He' },
+  { date: 3, text: 'Lithium', weight: 6.941, symbol: 'Li' },
+  // {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  // {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  // {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  // {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  // {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  // {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  // {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 
 @Component({
   selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  template: `./hello.compoent.html`,
+  styles: [`h1 { font-family: Lato; }`],
 })
-export class HelloComponent  {
-  @Input() name: string;
+export class HelloComponent {
+  columns = ['Date', 'Text'];
+}
+
+export class ExampleDataSource extends DataSource<PeriodicElement> {
+  /** Stream of data that is provided to the table. */
+  data = new BehaviorSubject<PeriodicElement[]>(MY_DATA);
+
+  /** Connect function called by the table to retrieve one stream containing the data to render. */
+  connect(): Observable<PeriodicElement[]> {
+    return this.data;
+  }
+
+  disconnect() {}
 }
